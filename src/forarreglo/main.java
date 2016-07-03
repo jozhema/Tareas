d33914f9cec50726c19828caad0480b55180ba27
jozhema <jozhema@jozhema>
2016-07-03 13:43:23 -0500

    vecinos

---
+++ src/forarreglo/main.java
@@ -0,0 +1,15 @@
+
+package forarreglo;
+
+/**
+ *
+ * @author juanpablo
+ */
+public class main {
+
+   
+    public static void main(String[] args) {
+        numerosvecinos fclas=new numerosvecinos();
+    
+    }    
+}
---
+++ src/forarreglo/numerosvecinos.java
@@ -0,0 +1,128 @@
+package forarreglo;
+
+
+import java.util.Random;
+
+
+
+public class numerosvecinos extends javax.swing.JFrame {
+    
+    private int[] numeroAleatorio;
+    
+    
+     public numerosvecinos() {
+         
+     }
+     public void datos(){
+         numeroAleatorio[0]=4;
+         numeroAleatorio[1]=4;
+         numeroAleatorio[2]=4;
+         numeroAleatorio[3]=4;
+         numeroAleatorio[4]=4;
+         numeroAleatorio[5]=4;
+         numeroAleatorio[6]=4;
+         numeroAleatorio[7]=4;
+         numeroAleatorio[8]=4;
+         numeroAleatorio[9]=4;
+         numeroAleatorio[10]=4;
+         numeroAleatorio[11]=4;
+         numeroAleatorio[12]=4;
+         numeroAleatorio[13]=4;
+         numeroAleatorio[14]=4;
+         numeroAleatorio[15]=4;
+        
+     }
+         {
+             
+        
+             
+             
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButton17 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton17.setText("jButton17");
+        jButton17.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton17ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("jLabel1");
+
+        jLabel2.setText("jLabel1");
+
+        jLabel3.setText("jLabel1");
+
+        jLabel4.setText("jLabel1");
+
+        jLabel5.setText("jLabel1");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(57, 57, 57)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 42, Short.MAX_VALUE)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(41, 41, 41)
+                        .addComponent(jButton17)))
+                .addContainerGap(393, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(50, 50, 50)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel5)
+                .addGap(87, 87, 87)
+                .addComponent(jButton17)
+                .addContainerGap(143, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton17ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton17ActionPerformed
+     int numeroAleatorio = (int) (Math.random()*99+1);
+     System.out.println(" ="+ numeroAleatorio);
+     
+    }//GEN-LAST:event_jButton17ActionPerformed
+
+   
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton17;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    // End of variables declaration//GEN-END:variables
+}
